3e84d0994676a368824121439c53cb3db75b941f
devaki <kamat.devaki1@gmail.com>
2019-03-05 21:19:41 -0800

    Changed URLParse

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ from time import time
 from sqlalchemy import create_engine
 import urllib
 
-
-
 app = Flask(__name__)
 app.secret_key = "Secret"
 
@@ -18,7 +16,7 @@ app.secret_key = "Secret"
 # cursor = connection.cursor()
 # print(cursor)
 
-params = urllib.parse.quote_plus("Driver={ODBC Driver 13 for SQL Server};Server=tcp:dvkc4.database.windows.net,1433;Database=dbc4;Uid=dvk@dvkc4;Pwd={Gmail2019!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+params = urllib.parse.urlparse("Driver={ODBC Driver 17 for SQL Server};Server=tcp:dvkc4.database.windows.net,1433;Database=dbc4;Uid=dvk@dvkc4;Pwd={Gmail2019!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
 engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
 # r = redis.StrictRedis(host='rdb3.redis.cache.windows.net', port=6380, db=0, password='iA3zVvBHpA+QJD1fPynGJ0gCr5qp4pv5fma8hUfi6MA=', ssl=True)
